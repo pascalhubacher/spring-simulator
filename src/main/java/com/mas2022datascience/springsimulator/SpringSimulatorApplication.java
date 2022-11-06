a1fe31cea0e3ea55400fa69e043ea83f4ca7cc50
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-11-06 11:41:51 +0100

    fix autowired warning

--- src/main/java/com/mas2022datascience/springsimulator/SpringSimulatorApplication.java
+++ src/main/java/com/mas2022datascience/springsimulator/SpringSimulatorApplication.java
@@ -17,7 +17,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -32,8 +31,11 @@ public class SpringSimulatorApplication implements CommandLineRunner {
 
 	final private static Logger LOG = LoggerFactory.getLogger(SpringSimulatorApplication.class);
 
-	@Autowired
-	private KafkaTracabProducer kafkaTracabProducer;
+	private final KafkaTracabProducer kafkaTracabProducer;
+
+	public SpringSimulatorApplication(KafkaTracabProducer kafkaTracabProducer) {
+		this.kafkaTracabProducer = kafkaTracabProducer;
+	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(SpringSimulatorApplication.class, args);
